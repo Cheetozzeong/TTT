c675c9481d13cfae9ea6d7a82b97f865bf63bde8
ych526 <ych526@naver.com>
2023-04-27 16:42:00 +0900

    🎨: 추가 AlarmLayout 구성 [S08P31A804-133]

--- tickletackletockle_wearable/app/src/main/java/com/ttt/tickletackletockle/presentation/AlarmScreen.kt
+++ tickletackletockle_wearable/app/src/main/java/com/ttt/tickletackletockle/presentation/AlarmScreen.kt
@@ -1,32 +1,22 @@
 package com.ttt.tickletackletockle.presentation
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.focusable
-import androidx.compose.foundation.gestures.animateScrollBy
-import androidx.compose.foundation.gestures.scrollBy
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.input.rotary.onPreRotaryScrollEvent
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.sp
 import androidx.wear.compose.material.*
 import com.ttt.tickletackletockle.R
 import com.ttt.tickletackletockle.complication.Tickle
 import com.ttt.tickletackletockle.theme.TTTTheme
-import kotlinx.coroutines.launch
 
 @Composable
 fun AlarmScreen(){
-    val tickle = Tickle(getEmojiByUnicode(0x1F95B),"10분마다 물 마시기")
+    //dummyData
+    val tickle = Tickle(getEmojiByUnicode(0x1F95B),"물 한잔 마시기!")
 
     TTTTheme {
         Scaffold(
@@ -39,35 +29,55 @@ fun AlarmScreen(){
                 Row(
                     Modifier
                         .fillMaxWidth()
-                        .weight(1.0f),
+                        .weight(0.3f),
                     verticalAlignment = Alignment.Bottom,
-                    horizontalArrangement = Arrangement.Center) {}
+                    horizontalArrangement = Arrangement.Center
+                ) {}
                 Row(
                     Modifier
                         .fillMaxWidth()
-                        .weight(1.0f),
-                    verticalAlignment = Alignment.Bottom,
+                        .weight(0.8f),
+                    verticalAlignment = Alignment.CenterVertically,
                     horizontalArrangement = Arrangement.Center,
                 ) {
                     Box(
-                        modifier = Modifier
-                            .border(
-                                width = 2.dp,
-                                color = MaterialTheme.colors.primary,
-                                shape = RoundedCornerShape(10.dp)
-                            )
-                            .size(110.dp, 30.dp),
+                        modifier = Modifier,
                         contentAlignment = Alignment.Center
                     ) {
-                        Text(text = stringResource(R.string.TickleListTitle))
+                        Text(text = tickle.emoji, fontSize = 50.sp)
                     }
                 }
                 Row(
                     Modifier
                         .fillMaxWidth()
-                        .weight(4.0f)
+                        .weight(0.5f),
+                    horizontalArrangement = Arrangement.Center
                 ) {
-
+                    Box(
+                        Modifier,
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            text = tickle.title ,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis,
+                        )
+                    }
+                }
+                Row(
+                    Modifier.weight(0.5f),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Button(
+                        onClick = { /*TODO*/ },
+                        Modifier
+                            .background(
+                                MaterialTheme.colors.primary,
+                            )
+                            .fillMaxSize(),
+                    ) {
+                        Text(text = stringResource(R.string.CheckOnAlarm), style = MaterialTheme.typography.body2)
+                    }
                 }
             }
         }
