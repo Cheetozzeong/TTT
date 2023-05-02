91da45e028de11db40d30e3997007f774def4a23
ych526 <ych526@naver.com>
2023-05-02 17:07:18 +0900

    test

--- tickletackletockle_wearable/app/src/main/java/com/a804/tickle_tackle_tockle/MainActivity.kt
+++ tickletackletockle_wearable/app/src/main/java/com/a804/tickle_tackle_tockle/MainActivity.kt
@@ -1,21 +1,28 @@
 package com.a804.tickle_tackle_tockle
 
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.a804.tickle_tackle_tockle.navigation.TTTNavHost
-import com.a804.tickle_tackle_tockle.presentation.AlarmScreen
-import com.a804.tickle_tackle_tockle.presentation.TickleListScreen
 import com.a804.tickle_tackle_tockle.theme.TTTTheme
 
 class MainActivity : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val bluetoothAdapter: BluetoothAdapter? by lazy(LazyThreadSafetyMode.PUBLICATION){
+            val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+            bluetoothManager.adapter
+        }
+
         setContent {
             TTTTheme {
                 TTTNavHost()
             }
         }
     }
+
+
 }
