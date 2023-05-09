4b44309ef123f774aa8a7aca0a2f368d8b26d3cf
이해은 <yihe4728@gmail.com>
2023-05-09 17:57:29 +0900

    🎨: 추가 홈화면, 리스트 화면UI[S08P31A804-371]

--- tickle_tackle_tockle/lib/screen/habits/habits_screen.dart
+++ tickle_tackle_tockle/lib/screen/habits/habits_screen.dart
@@ -1,14 +1,183 @@
 import 'package:flutter/material.dart';
+import 'package:tickle_tackle_tockle/const/theme.dart';
 import '../../component/common_appbar.dart';
+import '../../controller/loading_controller.dart';
+import '../../controller/theme_controller.dart';
+import 'package:get/get.dart';
+
 
 class HabitsScreen extends StatelessWidget {
   const HabitsScreen({Key? key}) : super(key: key);
 
+  buildWeekRow({
+    bool sun = false,
+    bool mon = false,
+    bool tue = false,
+    bool wed = false,
+    bool thu = false,
+    bool fri = false,
+    bool sat = false,
+    Color themeColor = TTTPrimary1
+  }) {
+    const double checkMaxRadius = 10;
+    const double checkMinRadius = 8;
+    const double checkFontSize = 10;
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        CircleAvatar(
+          backgroundColor: sun ? themeColor : TTTWhite,
+          maxRadius: checkMaxRadius,
+          child: const CircleAvatar(
+            backgroundColor: TTTWhite,
+            maxRadius: checkMinRadius,
+            child: Text('일', style: TextStyle(color: TTTPrimary1, fontSize: checkFontSize,)),
+          ),
+        ),
+        CircleAvatar(
+          backgroundColor: mon ? themeColor : TTTWhite,
+          maxRadius: checkMaxRadius,
+          child: const CircleAvatar(
+            backgroundColor: TTTWhite,
+            maxRadius: checkMinRadius,
+            child: Text('월', style: TextStyle(color: TTTBlack, fontSize: checkFontSize,)),
+          ),
+        ),
+        CircleAvatar(
+          backgroundColor: tue ? themeColor : TTTWhite,
+          maxRadius: checkMaxRadius,
+          child: const CircleAvatar(
+            backgroundColor: TTTWhite,
+            maxRadius: checkMinRadius,
+            child: Text('화', style: TextStyle(color: TTTBlack, fontSize: checkFontSize,)),
+          ),
+        ),
+        CircleAvatar(
+          backgroundColor: wed ? themeColor : TTTWhite,
+          maxRadius: checkMaxRadius,
+          child: const CircleAvatar(
+            backgroundColor: TTTWhite,
+            maxRadius: checkMinRadius,
+            child: Text('수', style: TextStyle(color: TTTBlack, fontSize: checkFontSize,)),
+          ),
+        ),
+        CircleAvatar(
+          backgroundColor: thu ? themeColor : TTTWhite,
+          maxRadius: checkMaxRadius,
+          child: const CircleAvatar(
+            backgroundColor: TTTWhite,
+            maxRadius: checkMinRadius,
+            child: Text('목', style: TextStyle(color: TTTBlack, fontSize: checkFontSize,)),
+          ),
+        ),
+        CircleAvatar(
+          backgroundColor: fri ? themeColor : TTTWhite,
+          maxRadius: checkMaxRadius,
+          child: const CircleAvatar(
+            backgroundColor: TTTWhite,
+            maxRadius: checkMinRadius,
+            child: Text('금', style: TextStyle(color: TTTBlack, fontSize: checkFontSize,)),
+          ),
+        ),
+        CircleAvatar(
+          backgroundColor: sat ? themeColor : TTTWhite,
+          maxRadius: checkMaxRadius,
+          child: const CircleAvatar(
+            backgroundColor: TTTWhite,
+            maxRadius: checkMinRadius,
+            child: Text('토', style: TextStyle(color: TTTPrimary1, fontSize: checkFontSize,)),
+          ),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    final Size size = MediaQuery.of(context).size;
+    final double deviceWidth = size.width;
+    final double deviceHeight = size.height;
+
+    LoadingController loadingController = Get.put(LoadingController());
+    ThemeController themeController = Get.put(ThemeController());
+
+    return Scaffold(
       appBar: CommonAppBar(appBarType: AppBarType.normalAppBar, title: '나의 습관 목록'),
-      body: Center(child: Text('HabitsScreen', style: TextStyle(fontSize: 30),)),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text('운동', style: TextStyle(fontSize: 25,)),
+                ],
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Container(
+                padding: EdgeInsets.all(20.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.7),
+                      blurRadius: 5.0,
+                      spreadRadius: 0.0,
+                      offset: const Offset(0, 7),
+                    ),
+                  ],
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(Icons.ac_unit_outlined),
+                    GetBuilder<ThemeController>(
+                      builder: (_) {
+                        return Column(
+                          children: [
+                            Text('달리기', style: TextStyle(fontSize: 18)),
+                            SizedBox(height: 5,),
+                            buildWeekRow(sun: true, mon: true, fri: true, themeColor: themeController.selectedPrimaryColor),
+                          ],
+                        );
+                      }
+                    ),
+                    GetBuilder<ThemeController>(
+                      builder: (_) {
+                        return Text('12 티끌', style: TextStyle(color: themeController.selectedPrimaryColor),);
+                      }
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
+
+/*children: [
+              Text('운동', style: TextStyle(fontSize: 25,)),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.7),
+                      blurRadius: 5.0,
+                      spreadRadius: 0.0,
+                      offset: const Offset(0, 7),
+                    ),
+                  ],
+                ),
+                child: Text('sd'),
+              ),
+            ],*/
