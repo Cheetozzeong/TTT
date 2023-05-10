78835832e75a9417af8b47ae1edb5255e47002e7
DESKTOP-DOGVPUB\SSAFY <parkhongbin95@gmail.com>
2023-05-10 10:41:43 +0900

    🚀: 새로운 기능 추가 - 알람 기능 추가, FCM 토큰 api 추가 [S08P31A804-360]

--- backend/src/main/java/com/a804/tictactoc/ttt/config/FirebaseConfig.java
+++ backend/src/main/java/com/a804/tictactoc/ttt/config/FirebaseConfig.java
@@ -4,6 +4,7 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.messaging.FirebaseMessaging;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
@@ -34,5 +35,10 @@ public class FirebaseConfig {
     @Bean
     public FirebaseAuth firebaseAuth() {
         return FirebaseAuth.getInstance();
+    }
+
+    @Bean
+    public FirebaseMessaging firebaseMessaging(){
+        return FirebaseMessaging.getInstance();
     }
 }
