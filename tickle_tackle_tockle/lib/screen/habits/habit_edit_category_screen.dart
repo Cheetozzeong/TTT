14a96695bfea6c0e893b4cc5e3de0f9070afd21f
이해은 <yihe4728@gmail.com>
2023-05-10 17:45:51 +0900

    🎨: 추가 알람 설정 UI 테스트 페이지[S08P31A804-343]

--- tickle_tackle_tockle/lib/screen/habits/habit_edit_category_screen.dart
+++ tickle_tackle_tockle/lib/screen/habits/habit_edit_category_screen.dart
@@ -11,35 +11,37 @@ class HabitEditCategoryScreen extends StatelessWidget {
     final double deviceWidth = size.width;
     final double deviceHeight = size.height;
 
-    return Scaffold(
-      appBar: CommonAppBar(appBarType: AppBarType.normalAppBar, title: '카테고리 선택'),
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(height: deviceHeight * 0.2,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                SizedBox(width: deviceWidth * 0.47, child: Image.asset('assets/images/buttons/categoryBtnIcon_money.png'),),
-                SizedBox(width: deviceWidth * 0.47, child: Image.asset('assets/images/buttons/categoryBtnIcon_exercise.png'),),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                SizedBox(width: deviceWidth * 0.47, child: Image.asset('assets/images/buttons/categoryBtnIcon_study.png'),),
-                SizedBox(width: deviceWidth * 0.47, child: Image.asset('assets/images/buttons/categoryBtnIcon_relationship.png'),),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                SizedBox(width: deviceWidth * 0.47, child: Image.asset('assets/images/buttons/categoryBtnIcon_life.png'),),
-                SizedBox(width: deviceWidth * 0.47, child: Image.asset('assets/images/buttons/categoryBtnIcon_etc.png'),),
-              ],
-            ),
-          ],
+    return SafeArea(
+      child: Scaffold(
+        appBar: CommonAppBar(appBarType: AppBarType.normalAppBar, title: '카테고리 선택'),
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: deviceHeight * 0.2,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  SizedBox(width: deviceWidth * 0.47, child: Image.asset('assets/images/buttons/categoryBtnIcon_money.png'),),
+                  SizedBox(width: deviceWidth * 0.47, child: Image.asset('assets/images/buttons/categoryBtnIcon_exercise.png'),),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  SizedBox(width: deviceWidth * 0.47, child: Image.asset('assets/images/buttons/categoryBtnIcon_study.png'),),
+                  SizedBox(width: deviceWidth * 0.47, child: Image.asset('assets/images/buttons/categoryBtnIcon_relationship.png'),),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  SizedBox(width: deviceWidth * 0.47, child: Image.asset('assets/images/buttons/categoryBtnIcon_life.png'),),
+                  SizedBox(width: deviceWidth * 0.47, child: Image.asset('assets/images/buttons/categoryBtnIcon_etc.png'),),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
