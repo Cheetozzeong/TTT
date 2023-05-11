0f3ffcfd9d9efe46b6e1550c34cc52a0e1a09cbf
DESKTOP-DOGVPUB\SSAFY <parkhongbin95@gmail.com>
2023-05-11 13:56:45 +0900

    🚀: 새로운 기능 추가 - 워치 푸시 알림시 토큰 추가 [S08P31A804-376]

--- backend/src/main/java/com/a804/tictactoc/ttt/service/ScheduleServiceImpl.java
+++ backend/src/main/java/com/a804/tictactoc/ttt/service/ScheduleServiceImpl.java
@@ -6,7 +6,6 @@ import com.a804.tictactoc.ttt.db.entity.*;
 import com.a804.tictactoc.ttt.db.repository.*;
 import com.a804.tictactoc.ttt.request.PushReq;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.UserRecord;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.Message;
 import com.google.firebase.messaging.Notification;
@@ -14,10 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.time.LocalDate;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -56,13 +52,8 @@ public class ScheduleServiceImpl implements ScheduleService {
                     yoil = 0;
 
                 String targetTime = String.format("%02d%02d",now.getHour(), now.getMinute());
-
                 int finalYoil = yoil;
 
-                // 임시값
-//                targetTime = ""
-
-                // List<Habit> allHabit = habitRepo.findAll();
                 List<Habit> resq = habitRepo.findAll().stream()
                         .filter(habit -> habit.getRepeatDay().substring(finalYoil,finalYoil + 1).equals("1")    // 울리는 요일 맞는지
                                     && habit.getDeleteYn() == 0                                     // 삭제 안되었는지
@@ -75,32 +66,38 @@ public class ScheduleServiceImpl implements ScheduleService {
                 List<PushReq> pushList = new ArrayList<>();
 
                 int successCount = 0;
+                int failCount = 0;
 
                 for (Habit habit:resq) {
                     pushList.add(new PushReq(habit.getName(),habit.getEmoji(), habit.getUserId()));
-//                    System.out.println(pushList.get(i++).getEmoji());
                     // 유저 정보 : 현재는 하나하나 가져오게 되어있는데 추후에 한번에 가져와서 찾는 방식으로 개편 필요하다
                     User selectedUser = userRepo.findById(habit.getUserId()).get();
+                    System.out.println(selectedUser.getWatchDeviceToken());
                     if(selectedUser != null
                             && selectedUser.getUid().isEmpty() == false
                             && selectedUser.getPhoneDeviceToken().isEmpty() == false){
-                        if(SendPush(habit.getEmoji(),habit.getName() + "할 시간입니다.",
-                                selectedUser.getPhoneDeviceToken(), CommonEnum.PushType.PHONE,selectedUser.getId()
-//                                "eAtQ0BURQr20zHcWxVFT33:APA91bFGAqIkAjKOe0VradxLkdow4ZMNqbslyAm7-KX5Eo6fU_QHVzD50C0tL9zRoF_Uiegjabghzm9enj-d9ZVv1_7z1wcGfGsu8QgWD_LZuTnGljZViLT0kv8fadU9G-y699a640bT"
-//                                "ckIc3tMUQV-JHJ6vtIogNd:APA91bEGOJT4Ov4mwhdV42yxohXlTDm1mj8Tn5R8YjzS3gA4mwZc2epEdDXNeIuW4CdMgAaSFaeZPZKmnOu-NpgVzvKyuoEsdUSkD3ocSNAE_tbCmiswFp2P7-JyWNKTpJZEQUpAQe70"
-                                )){
+                        if(SendPush(habit.getEmoji(),
+                                habit.getName() + "할 시간입니다.",
+                                selectedUser.getPhoneDeviceToken()
+                                ,CommonEnum.PushType.PHONE
+                                ,selectedUser.getId())){
                             if(selectedUser.getWatchDeviceToken().isEmpty() == false){
                                 //실패
-                                SendPush(habit.getEmoji(),habit.getName() + "할 시간입니다.",selectedUser.getWatchDeviceToken(), CommonEnum.PushType.WATCH,selectedUser.getId());
+                                SendPush(habit.getEmoji(),habit.getName() + "할 시간입니다."
+                                        ,selectedUser.getWatchDeviceToken()
+                                        ,CommonEnum.PushType.WATCH
+                                        ,selectedUser.getId());
                             }
                             successCount++;
                         }
                         else{
+                            failCount++;
                         }
 
                     }
                 }
                 result = new CommonResult(CommonEnum.Result.SUCCESS, successCount + "개의 메세지 전송에 성공했습니다.");
+                System.out.println(successCount + "개의 메세지 전송에 성공/" + failCount + "개의 매세지 전송에 실패 했습니다.");
 
             }
         }
@@ -131,9 +128,13 @@ public class ScheduleServiceImpl implements ScheduleService {
                     .setTitle(title)
                     .setBody(sendMessage)
                     .build();
+
             Message message = Message
                     .builder()
                     .putData("score","이치헌머하삼?ㅋㅋㅋㅋㅋ푸하하하")
+                    .putData("accessToken","치헌아 accessToken받고싶어??")
+                    .putData("refreshToken","refreshToken줄까?? 받고싶어??")
+                    .putData("happy","✨✨✨✨✨")
                     .setNotification(notification)
                     .setToken(deviceToken)
                     .build();
@@ -146,6 +147,7 @@ public class ScheduleServiceImpl implements ScheduleService {
         }
         catch (Exception ex){
             result = false;
+            System.out.println(ex.toString());
         }
 
         return result;
