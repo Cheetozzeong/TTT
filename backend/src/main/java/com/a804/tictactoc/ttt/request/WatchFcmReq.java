88ba4dd439627eeeaa9f11c90edca806c5ffd483
DESKTOP-DOGVPUB\SSAFY <parkhongbin95@gmail.com>
2023-05-11 17:53:06 +0900

    🚀: 새로운 기능 추가 - 워치 토큰 변경시 알람 가능 여부 추가 [S08P31A804-376]

--- backend/src/main/java/com/a804/tictactoc/ttt/request/WatchFcmReq.java
+++ backend/src/main/java/com/a804/tictactoc/ttt/request/WatchFcmReq.java
@@ -1,8 +1,11 @@
 package com.a804.tictactoc.ttt.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
 public class WatchFcmReq {
     String watchFcmToken;
+    @Schema(name="워치에 완료 알람 보내면 true, 안보내면 false")
+    boolean sendPush;
 }
