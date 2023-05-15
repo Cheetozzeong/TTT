2188c746314522951624835f440ee9efdf17eada
yihe4728 <yihe4728@gmail.com>
2023-05-16 03:23:53 +0900

    🚀: 추가 습관 생성 페이지 알람 설정 페이지[S08P31A804-359]

--- remake/tickle_tackle_tockle/lib/controller/create_habit_controller.dart
+++ remake/tickle_tackle_tockle/lib/controller/create_habit_controller.dart
@@ -5,11 +5,27 @@ class CreateHabitController extends GetxController {
   String repeatWeek = '1000000';
   String category = '금전';
   String name = '';
+  bool isAlarmRepeat = false;
+  String alarmTime = '0000';
+  String startTime = '0000';
+  String endTime = '0000';
+  String repeatTime = '0000';
+
 
   initValue() {
     emoji = '😀';
     repeatWeek = '1000000';
     category = '금전';
     name = '';
+    isAlarmRepeat = false;
+    alarmTime = '0000';
+    startTime = '0000';
+    endTime = '0000';
+    repeatTime = '0000';
+  }
+
+  changeAlarmState(bool value) {
+    isAlarmRepeat = value;
+    update();
   }
 }
